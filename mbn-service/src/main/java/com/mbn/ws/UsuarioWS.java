db37b4b4ab344298e93e624e81bf048c6f6a86c8
Annel Gonzalez <annelkaren@gmail.com>
2017-07-10 16:57:49 -0500

    Cambio de nombrado de paquete

---
+++ mbn-service/src/main/java/com/mbn/ws/UsuarioWS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mbn.ws;
+
+import com.mbn.model.dto.UsuarioDTO;
+import com.mbn.model.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author annelkaren
+ */
+
+@RestController
+@RequestMapping("/ws")
+public class UsuarioWS {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @RequestMapping(value = "/iniciarSesion/", method = RequestMethod.POST)
+    @ResponseBody
+    public UsuarioDTO guardarIcono( @RequestBody UsuarioDTO datos) {
+       return usuarioService.iniciarSesion(datos.getUsuario().getUsuario(), datos.getUsuario().getContrasena());
+    }
+    
+}
